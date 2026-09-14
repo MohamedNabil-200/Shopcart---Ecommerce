972d020d83dee43002bd7f4f214b1f63198b0107
Mohamed Nabil <mohamed.nabil67999@gmail.com>
2026-09-14 10:15:41 +0300

    feat: creating home banner and added all the pics

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
 import Container from "@/components/Container";
+import HomeBanner from "@/components/HomeBanner";
 
 const page = () => {
   return (
     <Container>
-      <h2>page</h2>{" "}
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-        deserunt quasi quisquam corporis qui eius fugit, cupiditate cum, eaque
-        temporibus dolore tempore velit! Natus laboriosam delectus ipsam, rerum
-        pariatur modi, sed reiciendis deleniti aliquid voluptatum possimus
-        recusandae. Neque doloribus minima soluta fugiat modi, minus iusto
-        maiores, nam aliquam eaque officiis.
-      </p>
+      <HomeBanner />
     </Container>
   );
 };
---
+++ images/index.ts
@@ -0,0 +1,5 @@
+import paypalLogo from "@/images/paypalLogo.png";
+import emptyCart from "@/images/emptyCart.png";
+import banner_1 from "@/images/banner/banner_1.png";
+
+export { paypalLogo, emptyCart, banner_1 };
