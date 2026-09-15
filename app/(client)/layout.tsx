67002c437f8a348ac990b08261f6a116c0ede4f3
Mohamed Nabil <mohamed.nabil67999@gmail.com>
2026-09-15 16:18:38 +0300

    feat: setup sanity and creating datasets

---
+++ app/(client)/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import "../globals.css";
+import { Figtree } from "next/font/google";
+import { cn } from "@/lib/utils";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+const figtree = Figtree({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: "Shopcart online store",
+  description: "Shopcart online store, you one stop shop for all you needs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" className={cn("font-sans", figtree.variable)}>
+        <body>
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,11 @@
-import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
-import { Figtree } from "next/font/google";
-import { cn } from "@/lib/utils";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-
-const figtree = Figtree({ subsets: ["latin"], variable: "--font-sans" });
-
-export const metadata: Metadata = {
-  title: "Shopcart online store",
-  description: "Shopcart online store, you one stop shop for all you needs",
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body className="min-h-full flex flex-col font-poppins antialiased">
+        {children}
+      </body>
+    </html>
+  );
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <ClerkProvider>
-      <html lang="en" className={cn("font-sans", figtree.variable)}>
-        <body className="min-h-full flex flex-col font-poppins antialiased">
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1">{children}</main>
-            <Footer />
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
-}
+export default RootLayout;
