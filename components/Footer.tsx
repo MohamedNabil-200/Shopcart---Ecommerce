5e698d73d4acb0f60f57c791106056eb6de45311
Mohamed Nabil <mohamed.nabil67999@gmail.com>
2026-09-15 08:52:35 +0300

    feat: creating footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,82 @@
+import { categoriesData, quickLinksData } from "@/constants/data";
 import Container from "./Container";
+import FooterTop from "./FooterTop";
+import Logo from "./Logo";
+import SocialMedia from "./SocialMedia";
+import { SubText, SubTitle } from "./ui/Text";
+import Link from "next/link";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 const Footer = () => {
   return (
-    <footer>
-      <Container>Footer</Container>
+    <footer className="bg-white border-t">
+      <Container>
+        <FooterTop />
+        <div className="py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="space-y-4">
+            <Logo />
+            <SubText>
+              Discover curated furniture collections at Shopcartyt, blending
+              style and comfort to elevate your living spaces.
+            </SubText>
+            <SocialMedia
+              className="text-dark-color/60"
+              iconClassName="border-dark-color/60 hover:border-shop-light-green hover:text-shop-light-green"
+              tooltipClassName="bg-dark-color text-white"
+            />
+          </div>
+          <div>
+            <SubTitle>Quick Links</SubTitle>
+            <ul className="space-y-3 mt-4">
+              {quickLinksData.map((item) => (
+                <li key={item.title}>
+                  <Link
+                    href={item.href}
+                    className="hover:text-shop-light-green hoverEffect font-medium"
+                  >
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <SubTitle>Categories</SubTitle>
+            <ul className="space-y-3 mt-4">
+              {categoriesData.map((item) => (
+                <li key={item.title}>
+                  <Link
+                    href={`category/${item.href}`}
+                    className="hover:text-shop-light-green hoverEffect font-medium"
+                  >
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="space-y-4">
+            <SubTitle>New Letter</SubTitle>
+            <SubText>
+              Subscribe to out newsletter to receive updates and exclusive
+              offers
+            </SubText>
+            <form className="space-y-3">
+              <Input placeholder="Enter your Email" type="email" required />
+              <Button className="w-full" type="submit">
+                Subscribe
+              </Button>
+            </form>
+          </div>
+        </div>
+        <div className="py-6 border-t text-center text-sm text-gray-600">
+          <div>
+            © {new Date().getFullYear()} <Logo className="text-sm" />. All
+            rights reserved.
+          </div>
+        </div>
+      </Container>
     </footer>
   );
 };
