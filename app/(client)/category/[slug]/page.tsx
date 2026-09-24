240fbf30b16bc3f6bae0a4d8f8d951b5c75dbc0c
Mohamed Nabil <mohamed.nabil67999@gmail.com>
2026-09-24 16:20:37 +0300

    feat(category): creating category page

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -1,5 +1,28 @@
-const CategoryPage = () => {
-  return <div>CategoryPage</div>;
+import CategoryProduct from "@/components/CategoryProduct";
+import Container from "@/components/Container";
+import { Title } from "@/components/ui/Text";
+import { getCategories } from "@/sanity/queries";
+
+const CategoryPage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const categories = await getCategories();
+  const { slug } = await params;
+  return (
+    <div className="py-10">
+      <Container>
+        <Title>
+          Products by Category:{" "}
+          <span className="font-bold text-green-600 capitalize tracking-wide">
+            {slug && slug}
+          </span>
+        </Title>
+        <CategoryProduct categories={categories} slug={slug}/>
+      </Container>
+    </div>
+  );
 };
 
 export default CategoryPage;
